b6efd83b9db2048d88a6e8a8f55eeb3e8f524c21
git-infinianti <infante.joshua1994@gmail.com>
2023-08-16 08:46:17 -0700

    Tried to be clever. Failed, retrying correctness.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main(): 
-    try: print('Hello, Python!'); raise(LookupError)
-    except: print('Goodbye, Python.\nHello World'); return
+    try: print('Hello World, Welcome to Python!'); raise(LookupError)
+    except: print('Goodbye World.')
     finally: print('Thank you, for using Python!')
 
 if __name__ == '__main__': main()
