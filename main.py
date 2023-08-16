4b93709240fc69bb425237f24b7ae68bf3e9200b
git-infinianti <infante.joshua1994@gmail.com>
2023-08-16 08:26:01 -0700

    Added Correctness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main(): 
     try: print('Hello, Python!'); raise(LookupError)
-    except: print('Goodbye, Python.'); return
+    except: print('Goodbye, Python.\nHello World'); return
     finally: print('Thank you, for using Python!')
 
 if __name__ == '__main__': main()
